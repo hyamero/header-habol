f062b3909a98452ee03717bcf54f4521f1fa4ef1
Dale B <daleban.dev@gmail.com>
2021-08-16 21:51:04 +0800

    Revert "Add sass files."
    This reverts commit be493e7a6a3618a626970073e86470a6b3e18ae7.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-/**@jsxRuntime classic */
 /**@jsx jsx */
 import { css, jsx, Global } from "@emotion/react";
 import React from "react";
-import "./styles.css";
+import CondensedDisplay from "./fonts/Moniqa-CondensedDisplay.otf";
+import MediumCondensedDisplay from "./fonts/Moniqa-MediumCondensedDisplay.otf";
+import BoldCondensedDisplay from "./fonts/Moniqa-BoldCondensedDisplay.otf";
+import BlackCondensedDisplay from "./fonts/Moniqa-BlackCondensedDisplay.otf";
+import background from "./img/katsiaryna-endruszkiewicz-20SYlxMNiQA-unsplash.jpg";
 
 function App() {
   return (
     <div
       className="App"
       css={css`
+        height: 100vh;
+        background: url(${background});
+        background-size: cover;
+
         p {
           font-size: 10rem;
           font-family: moniqa;
@@ -22,30 +29,35 @@ function App() {
         styles={css`
           @font-face {
             font-family: moniqa;
-            src: url("../fonts/Moniqa-CondensedDisplay.otf");
+            src: url(${CondensedDisplay});
             font-style: normal;
             font-weight: 200;
           }
 
           @font-face {
-            font-family: "moniqa";
-            src: url("./fonts/Moniqa-MediumCondensedDisplay.otf");
+            font-family: moniqa;
+            src: url(${MediumCondensedDisplay});
             font-style: normal;
             font-weight: 400;
           }
 
           @font-face {
             font-family: moniqa;
-            src: url("../fonts/Moniqa-BoldCondensedDisplay.otf");
+            src: url(${BoldCondensedDisplay});
             font-style: normal;
             font-weight: 600;
           }
 
           @font-face {
             font-family: moniqa;
-            src: url("../fonts/Moniqa-BlackCondensedDisplay.otf");
+            src: url(${BlackCondensedDisplay});
             font-style: normal;
             font-weight: 800;
+          }
+
+          * {
+            margin: 0;
+            padding: 0;
           }
         `}
       />
