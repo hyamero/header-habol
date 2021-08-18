b334c18aef9b5396d23711d48b3d24e897a642bb
Dale B <daleban.dev@gmail.com>
2021-08-18 22:02:14 +0800

    Initialize content design.

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -9,8 +9,12 @@ const Content = () => {
       css={css`
         font-family: moniqa;
         font-weight: 200;
-        font-size: 6rem;
+        font-size: 10rem;
         color: #dcedf2;
+        height: 85vh;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
 
         h1 {
           height: 80%;
@@ -23,13 +27,42 @@ const Content = () => {
         p {
           line-height: 0.8;
         }
+
+        .section-text {
+          width: 30vw;
+          text-align: left;
+          position: relative;
+          top: 8rem;
+
+          h3 {
+            font-size: 4rem;
+            font-weight: 200;
+          }
+
+          p {
+            font-family: "Montserrat", sans-serif;
+            font-size: 0.8rem;
+            font-weight: 400;
+            line-height: 1.1rem;
+            opacity: 0.8;
+            width: 70%;
+          }
+        }
       `}
     >
       <div className="main-text">
         <p>
-          BLANKET
-          <div className="stxt">THE CENTURY</div>
           HABOLEON
+          <div className="stxt">THE CENTURY</div>
+          BLANKETOIS
+        </p>
+      </div>
+      <div className="section-text">
+        <h3>HABOLEON, WHAT IS IT?</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente id
+          recusandae unde, vel dolores quasi reprehenderit ipsam. Doloribus
+          recusandae voluptatibus quia minus est? Lorem ipsum dolor sit amet.
         </p>
       </div>
     </section>
